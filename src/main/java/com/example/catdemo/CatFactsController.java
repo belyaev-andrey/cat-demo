32046c755e4244faff86ba6121696589e627c8c1
Andrey Belyaev <belyaev.andrey@gmail.com>
2026-02-09 19:50:41 +0100

    Minor cleanup

--- src/main/java/com/example/catdemo/CatFactsController.java
+++ src/main/java/com/example/catdemo/CatFactsController.java
@@ -7,8 +7,6 @@ import org.springframework.resilience.annotation.Retryable;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.service.annotation.GetExchange;
 import org.springframework.web.service.registry.ImportHttpServices;
 
